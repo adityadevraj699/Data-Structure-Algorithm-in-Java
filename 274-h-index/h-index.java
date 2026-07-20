ad644296bbf857cc6d033c096efdf409a6f815fe
ADITYA KUMAR <137451161+adityadevraj699@users.noreply.github.com>
2026-07-20 18:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (85.25%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hIndex(int[] citations) {
+       int[] bucket = new int[citations.length+1];
+       int count=0;
+
+       for(int i=0;i<citations.length;i++){
+        if(citations[i]>citations.length){
+            bucket[citations.length]++;
+        }
+        else{
+            bucket[citations[i]]++;
+        }
+       }
+
+       for(int i=bucket.length-1;i>=0;i--){
+        count +=bucket[i];
+        if(count>=i){
+            return i;
+        }
+       }
+
+       return count;
+
+
+        
+    }
+}
