a87cd9c9892ba36684149f205bca0860672a0e1e
ADITYA KUMAR <137451161+adityadevraj699@users.noreply.github.com>
2026-07-22 20:40:08 +0530

    Time: 3 ms (59.51%) | Memory: 44.4 MB (56.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,8 +3,8 @@ class Solution {
 
         int start=0,end=s.length()-1;
 
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
+        // StringBuilder sb1 = new StringBuilder();
+        // StringBuilder sb2 = new StringBuilder();
 
         if(s.isEmpty()) return true;
         if(s.length()-1 <1) return true;
@@ -21,9 +21,13 @@ class Solution {
             }
             }
             if(start<end){
-                sb1.append(Character.toLowerCase(s.charAt(start)));
-                sb2.append(Character.toLowerCase(s.charAt(end)));
-                 if(!(sb1.toString().equals(sb2.toString()))){
+            //     sb1.append(Character.toLowerCase(s.charAt(start)));
+            //     sb2.append(Character.toLowerCase(s.charAt(end)));
+            //      if(!(sb1.toString().equals(sb2.toString()))){
+            //     return false;
+            // }
+
+            if(Character.toLowerCase(s.charAt(start))!=(Character.toLowerCase(s.charAt(end)))){
                 return false;
             }
             }
