7362753cf43dd1032eac8454b9bc0eca74be9bfc
ADITYA KUMAR <137451161+adityadevraj699@users.noreply.github.com>
2026-07-23 17:00:55 +0530

    Time: 30 ms (88.60%) | Memory: 60.1 MB (14.06%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -8,9 +8,10 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
             
-            while(i<nums.length && i!=0 && nums[i]==nums[i-1]){
-                i++;
-            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+    }
+    
             int j=i+1, k=nums.length-1;
             while(j<k){
                 int sum = nums[i]+nums[j]+nums[k];
