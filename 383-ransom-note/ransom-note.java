cbb8890a3d6553f36914c98f3d0c239a49a7c5cf
ADITYA KUMAR <137451161+adityadevraj699@users.noreply.github.com>
2026-07-27 17:39:26 +0530

    Time: 1 ms (99.98%) | Memory: 46.4 MB (57.11%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -3,18 +3,26 @@ class Solution {
 
         if (magazine.length() < ransomNote.length()) return false;
 
-       List<Character> list = new ArrayList<>();
-       for(char ch : magazine.toCharArray()){
-        list.add(ch);
-       }
-       for(char ch : ransomNote.toCharArray()){
-        if(list.contains(ch)){
-            list.remove(Character.valueOf(ch));
-        }
-        else{
-            return false;
-        }
-       }
+    //    List<Character> list = new ArrayList<>();
+    //    for(char ch : magazine.toCharArray()){
+    //     list.add(ch);
+    //    }
+    //    for(char ch : ransomNote.toCharArray()){
+    //     if(list.contains(ch)){
+    //         list.remove(Character.valueOf(ch));
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    //    }
+
+    int[] freq = new int[26];
+
+    for(char ch : ransomNote.toCharArray()){
+        int i= magazine.indexOf(ch, freq[ch -'a']);
+        if(i == -1) return false;
+        freq[ch -'a'] = i+1;
+    }
 
        return true;
     }
