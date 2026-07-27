4c7abd7dd8066d57e27f6db3cfa204bd5e25c98b
ADITYA KUMAR <137451161+adityadevraj699@users.noreply.github.com>
2026-07-27 18:13:08 +0530

    Time: 11 ms (68.05%) | Memory: 44.2 MB (10.92%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -4,14 +4,16 @@ class Solution {
 
 
        HashMap<Character,Character> map = new HashMap<>();
+       HashSet<Character> set = new HashSet<>();
 
        for(int i=0;i<s.length(); i++){
         if(map.containsKey(s.charAt(i))){
             if(t.charAt(i) != map.get(s.charAt(i))) return false;
         }
         else{
-            if(map.containsValue(t.charAt(i))) return false;
+            if(set.contains(t.charAt(i))) return false;
             map.put(s.charAt(i), t.charAt(i));
+            set.add(t.charAt(i));
         }
        }
 
