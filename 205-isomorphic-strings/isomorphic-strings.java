340cbafe99514c52d720ea479a6b6b87d3f8cf46
ADITYA KUMAR <137451161+adityadevraj699@users.noreply.github.com>
2026-07-27 18:15:42 +0530

    Time: 5 ms (98.48%) | Memory: 44.1 MB (16.19%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,22 +1,44 @@
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//        if(t.length() != s.length()) return false;
+
+
+//        HashMap<Character,Character> map = new HashMap<>();
+//        HashSet<Character> set = new HashSet<>();
+
+//        for(int i=0;i<s.length(); i++){
+//         if(map.containsKey(s.charAt(i))){
+//             if(t.charAt(i) != map.get(s.charAt(i))) return false;
+//         }
+//         else{
+//             if(set.contains(t.charAt(i))) return false;
+//             map.put(s.charAt(i), t.charAt(i));
+//             set.add(t.charAt(i));
+//         }
+//        }
+
+//        return true; 
+//     }
+// }
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-       if(t.length() != s.length()) return false;
 
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
 
-       HashMap<Character,Character> map = new HashMap<>();
-       HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
 
-       for(int i=0;i<s.length(); i++){
-        if(map.containsKey(s.charAt(i))){
-            if(t.charAt(i) != map.get(s.charAt(i))) return false;
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            if (mapS[a] != mapT[b])
+                return false;
+
+            mapS[a] = i + 1;
+            mapT[b] = i + 1;
         }
-        else{
-            if(set.contains(t.charAt(i))) return false;
-            map.put(s.charAt(i), t.charAt(i));
-            set.add(t.charAt(i));
-        }
-       }
 
-       return true; 
+        return true;
     }
 }
